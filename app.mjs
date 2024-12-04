5fdd313136404526b406572dd69177aef97efffc
pu3mita <putrimitaa16@gmail.com>
2024-12-05 02:11:56 +0700

    Add files via upload

--- app.mjs
+++ app.mjs
@@ -3,6 +3,11 @@ import morgan from 'morgan';
 import cors from 'cors';
 import AcaraRoutes from './routes/AcaraRoutes.mjs';
 import ProfilRoutes from './routes/ProfilRoutes.mjs';
+import CategoriesRoutes from './routes/CategoriesRoutes.mjs';
+import FavoritesRoutes from './routes/FavoritesRoutes.mjs';
+import NotificationsRoutes from './routes/NotificationsRoutes.mjs';
+import PaymentsRoutes from './routes/PaymentsRoutes.mjs';
+import UsersRoutes from './routes/UsersRoutes.mjs';
 
 const app = express();
 const PORT = 5000;
@@ -20,6 +25,11 @@ app.get('/', (req, res, next) => {
 // Wrapping routing dg app.use()
 app.use('/api/v1/Profile', ProfilRoutes);
 app.use('/api/v1/Acara', AcaraRoutes);
+app.use('/api/v1/Kategori', CategoriesRoutes);
+app.use('/api/v1/Favorit', FavoritesRoutes);
+app.use('/api/v1/Notifikasi', NotificationsRoutes);
+app.use('/api/v1/Pembayaran', PaymentsRoutes);
+app.use('.api/v1/user', UsersRoutes)
 
 app.listen(PORT, () => {
   console.log(`Server is running on PORT ${PORT} http://localhost:${PORT}/`);
